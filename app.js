454c83c83551b222847864fb1f0cf0346579604a
Lee <winterfive@users.noreply.github.com>
2019-03-11 12:21:30 -0400

    Added handleClick(), onClick to all buttons

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ class MyApp extends React.Component {
     }
   }
   
+  this.handleCLick = this.handleCLick.bind(this);
+  
   clearAll() {
     
   }
   
   calculate() {
     // switch statement
+  }
+  
+  handleClick {
+    
   }
   
   
@@ -29,32 +35,32 @@ class MyApp extends React.Component {
         <div id="buttonDiv">
           <div>
             <div id="row1" class="row">
-              <div id="seven" class="numButton">7</div>
-              <div id="eight" class="numButton">8</div>
-              <div id="nine" class="numButton">9</div>
+              <div id="seven" class="numButton" onClick={this.handleClick}>7</div>
+              <div id="eight" class="numButton" onClick={this.handleClick}>8</div>
+              <div id="nine" class="numButton" onClick={this.handleClick}>9</div>
             </div>
             <div id="row2" class="row">
-              <div id="four" class="numButton">4</div>
-              <div id="five" class="numButton">5</div>
-              <div id="six" class="numButton">6</div>
+              <div id="four" class="numButton" onClick={this.handleClick}>4</div>
+              <div id="five" class="numButton" onClick={this.handleClick}>5</div>
+              <div id="six" class="numButton" onClick={this.handleClick}>6</div>
             </div>
             <div id="row3" class="row">
-              <div id="three" class="numButton">3</div>
-              <div id="two" class="numButton">2</div>
-              <div id="one" class="numButton">1</div>
+              <div id="three" class="numButton" onClick={this.handleClick}>3</div>
+              <div id="two" class="numButton" onClick={this.handleClick}>2</div>
+              <div id="one" class="numButton" onClick={this.handleClick}>1</div>
             </div>
             <div id="row4" class="row">
-              <div id="clear" class="numButton">C</div>
-              <div id="zero" class="numButton">0</div>
-              <div id="decimal" class="numButton">.</div>
+              <div id="clear" class="numButton" onClick={this.handleClick}>C</div>
+              <div id="zero" class="numButton" onClick={this.handleClick}>0</div>
+              <div id="decimal" class="numButton" onClick={this.handleClick}>.</div>
             </div>      
           </div>
           <div id="opsDiv">
-            <div id="divide" class="opsButton"><i class="fas fa-divide"></i></div>
-            <div id="multiply" class="opsButton"><i class="fas fa-times"></i></div>
-            <div id="subtract" class="opsButton"><i class="fas fa-minus"></i></div>
-            <div id="add" class="opsButton"><i class="fas fa-plus"></i></div>
-            <div id="equals" class="opsButton"><i class="fas fa-equals"></i></div>
+            <div id="divide" class="opsButton" onClick={this.handleClick}><i class="fas fa-divide"></i></div>
+            <div id="multiply" class="opsButton" onClick={this.handleClick}><i class="fas fa-times"></i></div>
+            <div id="subtract" class="opsButton" onClick={this.handleClick}><i class="fas fa-minus"></i></div>
+            <div id="add" class="opsButton" onClick={this.handleClick}><i class="fas fa-plus"></i></div>
+            <div id="equals" class="opsButton" onClick={this.handleClick}><i class="fas fa-equals"></i></div>
           </div> 
       </div>
         </div>
