50fde3b1aa07a4a704518bb577e33688e9b8a16c
Lee <winterfive@users.noreply.github.com>
2019-03-11 11:36:11 -0400

    Added calcDiv, opsDiv, and numDiv

--- app.js
+++ app.js
@@ -22,36 +22,38 @@ class MyApp extends React.Component {
   render() {
     return (
       <div>
-        <div id="numPanel">
-          <div id="row1" class="row">
-            <div id="seven" class="numButton">7</div>
-            <div id="eight" class="numButton">8</div>
-            <div id="nine" class="numButton">9</div>
+        <div id="calcDiv">
+          <div id="numDiv">
+            <div id="row1" class="row">
+              <div id="seven" class="numButton">7</div>
+              <div id="eight" class="numButton">8</div>
+              <div id="nine" class="numButton">9</div>
+            </div>
+            <div id="row2" class="row">
+              <div id="four" class="numButton">4</div>
+              <div id="five" class="numButton">5</div>
+              <div id="six" class="numButton">6</div>
+            </div>
+            <div id="row3" class="row">
+              <div id="three" class="numButton">3</div>
+              <div id="two" class="numButton">2</div>
+              <div id="one" class="numButton">1</div>
+            </div>
+            <div id="row4" class="row">
+              <div id="clear" class="numButton">C</div>
+              <div id="zero" class="numButton">0</div>
+              <div id="decimal" class="numButton">.</div>
+            </div>      
           </div>
-          <div id="row2" class="row">
-            <div id="four" class="numButton">4</div>
-            <div id="five" class="numButton">5</div>
-            <div id="six" class="numButton">6</div>
-          </div>
-          <div id="row3" class="row">
-            <div id="three" class="numButton">3</div>
-            <div id="two" class="numButton">2</div>
-            <div id="one" class="numButton">1</div>
-          </div>
-          <div id="row4" class="row">
-            <div id="clear" class="numButton">C</div>
-            <div id="zero" class="numButton">0</div>
-            <div id="decimal" class="numButton">.</div>
-          </div>      
-        </div>
-        <div id="col">
-          <div id="divide"><i class="fas fa-divide"></i></div>
-          <div id="multiply"><i class="fas fa-times"></i></div>
-          <div id="subtract"><i class="fas fa-minus"></i></div>
-          <div id="add"><i class="fas fa-plus"></i></div>
-          <div id="equals"><i class="fas fa-equals"></i></div>
+          <div id="opsDiv">
+            <div id="divide" class="opsButton"><i class="fas fa-divide"></i></div>
+            <div id="multiply" class="opsButton"><i class="fas fa-times"></i></div>
+            <div id="subtract" class="opsButton"><i class="fas fa-minus"></i></div>
+            <div id="add" class="opsButton"><i class="fas fa-plus"></i></div>
+            <div id="equals" class="opsButton"><i class="fas fa-equals"></i></div>
           </div> 
       </div>
+        </div>
     )
   }
 }
